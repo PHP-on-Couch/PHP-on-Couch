17ec626795dd5e16819b5d594d42051ee6271866
Clemens Sahs <clemens.sahs@slides-worker.org>
2013-12-11 12:22:21 +0100

    add a test for customCurlOptions

--- tests/couchHttpAdapterCurlTest.php
+++ tests/couchHttpAdapterCurlTest.php
@@ -31,4 +31,21 @@ class couchHttpAdapterCurlTest extends PHPUnit_Framework_TestCase
         );
         $response = $adapter->query($adapter::METHODE_PUT,$id,array(),$data);
     }
+
+    public function testCustomCurlOptions () {
+        $adapter = $this->adapter;
+        $adapterOptions(array('curl'=>array(CURLOPT_URL=>'http://www.example.com/')));
+
+        $addCustomOptions = new \ReflectionMethod($adapter, 'addCustomOptions');
+        $addCustomOptions->setAccessible(true);
+
+        $curlHandle= curl_init();
+
+        $adapter->setOptions($addCustomOptions);
+        $addCustomOptions->invoke($adapter,$curlHandle);
+
+        $info = curl_getinfo($ch);
+        $this->assertEquals($info['url'],$adapterOptions['curl'][CURLOPT_URL]);
+
+    }
 }
