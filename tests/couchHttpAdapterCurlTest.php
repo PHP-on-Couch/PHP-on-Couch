c366ff86028c3bb849dafc83d5d652d611dff329
Clemens Sahs <clemens.sahs@slides-worker.org>
2014-01-18 00:09:57 +0100

    push some code coverage to class „couchHttpAdapterCurl“

--- tests/couchHttpAdapterCurlTest.php
+++ tests/couchHttpAdapterCurlTest.php
@@ -22,17 +22,19 @@ class couchHttpAdapterCurlTest extends PHPUnit_Framework_TestCase
         $this->adapter = null;
     }
 
-    public function testAdapterSendToClient () {
+    public function testAdapterSendToClient ()
+    {
         $adapter = $this->adapter;
 
         $id = 'testid';
         $data = array(
             'foo'=>'bar',
         );
-        $response = $adapter->query($adapter::METHODE_PUT,$id,array(),$data);
+        $response = $adapter->query($adapter::METHODE_PUT, $id, array(), $data);
     }
 
-    public function testCustomCurlOptions () {
+    public function testCustomCurlOptions ()
+    {
         $adapter = $this->adapter;
         $adapterOptions=array('curl'=>array(CURLOPT_URL=>'http://www.example.com/'));
 
@@ -42,24 +44,28 @@ class couchHttpAdapterCurlTest extends PHPUnit_Framework_TestCase
         $curlHandle= curl_init();
 
         $adapter->setOptions($adapterOptions);
-        $addCustomOptions->invoke($adapter,$curlHandle);
+        $addCustomOptions->invoke($adapter, $curlHandle);
 
         $info = curl_getinfo($curlHandle);
-        $this->assertEquals($adapterOptions['curl'][CURLOPT_URL],$info['url']);
+        $this->assertEquals($adapterOptions['curl'][CURLOPT_URL], $info['url']);
 
     }
 
-    public function testBuildRequestSendCookie () {
+    public function testBuildRequestSendCookie ()
+    {
         $sessionCookie = "foo=bar";
 
 
         $adapter = $this->adapter;
         $adapter->setSessionCookie($sessionCookie);
 
+        $this->assertTrue($adapter->hasSessionCookie());
+        $this->assertEquals($sessionCookie, $adapter->getSessionCookie());
+
         $buildRequest = new \ReflectionMethod($adapter, 'buildRequest');
         $buildRequest->setAccessible(true);
 
-        $curlHandle = $buildRequest->invokeArgs($adapter,array(
+        $curlHandle = $buildRequest->invokeArgs($adapter, array(
             'COPY',
             'localhost:8080/_files/return_header.php',
             array('foo'=>'bar'),
@@ -67,10 +73,58 @@ class couchHttpAdapterCurlTest extends PHPUnit_Framework_TestCase
         ));
 
         curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
-        $header= json_decode(curl_exec($curlHandle),true);
+        $header= json_decode(curl_exec($curlHandle), true);
 
         $this->assertArrayHasKey('Cookie', $header);
         $this->assertEquals($sessionCookie, $header['Cookie']);
         $this->assertEquals($adapter->getSessionCookie(), $header['Cookie']);
+    }
+
+
+    public function testBuildRequestSendCustomContentType ()
+    {
+        $contentType = "foo/bar";
+
+        $adapter = $this->adapter;
+
+        $buildRequest = new \ReflectionMethod($adapter, 'buildRequest');
+        $buildRequest->setAccessible(true);
+
+        $curlHandle = $buildRequest->invokeArgs($adapter, array(
+            'COPY',
+            'localhost:8080/_files/return_header.php',
+            array('foo'=>'bar'),
+            $contentType
+        ));
+
+        curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
+        $header= json_decode(curl_exec($curlHandle), true);
+
+        $this->assertArrayHasKey('Content-Type', $header);
+        $this->assertEquals($contentType, $header['Content-Type']);
+    }
+
+
+    public function testBuildRequestSendDefaultContentType ()
+    {
+        $defaultContentType = "application/json";
+
+        $adapter = $this->adapter;
+
+        $buildRequest = new \ReflectionMethod($adapter, 'buildRequest');
+        $buildRequest->setAccessible(true);
+
+        $curlHandle = $buildRequest->invokeArgs($adapter, array(
+            'COPY',
+            'localhost:8080/_files/return_header.php',
+            array('foo'=>'bar'),
+            null
+        ));
+
+        curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
+        $header= json_decode(curl_exec($curlHandle), true);
+
+        $this->assertArrayHasKey('Content-Type', $header);
+        $this->assertEquals($defaultContentType, $header['Content-Type']);
     }
 }
