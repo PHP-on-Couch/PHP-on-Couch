a29128db764f71fc33fc80fd88843d7e17ba4c8c
Clemens Sahs <clemens.sahs@slides-worker.org>
2014-01-23 11:43:20 +0100

    Coding Standart fix

--- lib/couchHttpAdapterAbstract.php
+++ lib/couchHttpAdapterAbstract.php
@@ -1,54 +1,76 @@
 <?php
 
-abstract class couchHttpAdapterAbstract implements couchHttpAdapterInterface {
+abstract class couchHttpAdapterAbstract implements couchHttpAdapterInterface
+{
+
     protected $dsn = null;
+
     protected $options = null;
+
     /**
-    * @var array allowed HTTP methods for REST dialog
-    */
-    protected $HTTP_METHODS = array(SELF::METHODE_PUT,SELF::METHODE_POST,SELF::METHODE_GET,SELF::METHODE_DELETE,SELF::METHODE_COPY);
+     *
+     * @var array allowed HTTP methods for REST dialog
+     */
+    protected $HTTP_METHODS = array(
+        SELF::METHODE_PUT,
+        SELF::METHODE_POST,
+        SELF::METHODE_GET,
+        SELF::METHODE_DELETE,
+        SELF::METHODE_COPY
+    );
 
-	/**
-	* @var string the session cookie
-	*/
-	protected $sessioncookie = null;
+    /**
+     *
+     * @var string the session cookie
+     */
+    protected $sessioncookie = null;
 
-    public function setDsn ($dsn) {
-        $this->dsn= $dsn;
+    public function setDsn($dsn)
+    {
+        $this->dsn = $dsn;
     }
-    public function getDsn () {
+
+    public function getDsn()
+    {
         return $this->dsn;
     }
 
-    public function setOptions ($options) {
+    public function setOptions($options)
+    {
         $this->options = $options;
     }
 
-    public function getOptions () {
+    public function getOptions()
+    {
         return $this->options;
     }
 
-    public function __construct($options) {
+    public function __construct($options)
+    {
         $this->setOptions($options);
     }
 
-	/**
-	* set the session cookie to send in the headers
-	* @param string $cookie the session cookie ( example : AuthSession=Y291Y2g6NENGNDgzNz )
-	*
-	* @return \couch
-	*/
-	public function setSessionCookie ( $cookie ) {
-		$this->sessioncookie = $cookie;
-		return $this;
-	}
+    /**
+     * set the session cookie to send in the headers
+     *
+     * @param string $cookie
+     *            the session cookie ( example : AuthSession=Y291Y2g6NENGNDgzNz )
+     *
+     * @return \couch
+     */
+    public function setSessionCookie($cookie)
+    {
+        $this->sessioncookie = $cookie;
+        return $this;
+    }
 
     /**
      * get the session cookie
      *
      * @return string cookie
      */
-    public function getSessionCookie () {
+    public function getSessionCookie()
+    {
         return $this->sessioncookie;
     }
 
@@ -57,12 +79,16 @@ abstract class couchHttpAdapterAbstract implements couchHttpAdapterInterface {
      *
      * @return string cookie
      */
-    public function hasSessionCookie () {
+    public function hasSessionCookie()
+    {
         return (bool) $this->sessioncookie;
     }
 
-    abstract public function query ( $method, $url, $parameters = array() , $data = NULL, $content_type = NULL );
-    abstract public function storeAsFile($url,$data,$content_type);
-    abstract public function storeFile($url,$file,$content_type);
-    abstract public function continuousQuery($callable,$method,$url,$parameters = array(),$data = null);
+    abstract public function query($method, $url, $parameters = array(), $data = NULL, $content_type = NULL);
+
+    abstract public function storeAsFile($url, $data, $content_type);
+
+    abstract public function storeFile($url, $file, $content_type);
+
+    abstract public function continuousQuery($callable, $method, $url, $parameters = array(), $data = null);
 }
